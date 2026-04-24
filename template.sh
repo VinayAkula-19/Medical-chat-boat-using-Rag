638305c51f708ca87ee29bd33cc47357fef98663
VinayAkula-19 <vinayakula345@gmail.com>
2026-04-24 11:50:33 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#directory
+mkdir -p src
+mkdir -p research
+
+#files
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials/ipynb
+touch requirments.txt
+
+echo " files created successfully"
